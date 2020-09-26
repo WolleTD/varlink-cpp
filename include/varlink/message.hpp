1b97beb494f79dcd60955969a07c3e77e8201cf3
Eicke Herbertz <wolletd@posteo.de>
2020-09-26 12:59:52 +0200

    Remove inheritance from Message

--- include/varlink/message.hpp
+++ include/varlink/message.hpp
@@ -9,28 +9,29 @@
 
 namespace varlink {
 
-    class Message : json {
+    class Message {
+        json json_;
         bool hasmore{false};
         bool hasoneway{false};
     public:
         Message() = default;
-        explicit Message(const json& msg) : json(msg) {
-            if (!is_object() || !contains("method") || !find("method")->is_string()) {
+        explicit Message(const json& msg) : json_(msg) {
+            if (!json_.is_object() || !json_.contains("method") || !json_["method"].is_string()) {
                 throw std::invalid_argument(msg.dump());
             }
-            if (!contains("parameters")) {
-                emplace("parameters", json::object());
-            } else if(!find("parameters")->is_object()) {
-                throw(std::invalid_argument(msg.dump()));
+            if (!json_.contains("parameters")) {
+                json_.emplace("parameters", json::object());
+            } else if(!json_["parameters"].is_object()) {
+                throw(std::invalid_argument("Not a varlink message: " + msg.dump()));
             }
             hasmore = (msg.contains("more") && msg["more"].get<bool>());
             hasoneway = (msg.contains("oneway") && msg["oneway"].get<bool>());
         }
         [[nodiscard]] bool more() const { return hasmore; }
         [[nodiscard]] bool oneway() const { return hasoneway; }
-        [[nodiscard]] const json& parameters() const { return *find("parameters"); }
+        [[nodiscard]] const json& parameters() const { return json_["parameters"]; }
         [[nodiscard]] std::pair<std::string, std::string> interfaceAndMethod() const {
-            const auto& fqmethod = find("method")->get<std::string>();
+            const auto& fqmethod = json_["method"].get<std::string>();
             const auto dot = fqmethod.rfind('.');
             // When there is no dot at all, both fields contain the same value, but it's an invalid
             // interface name anyway
