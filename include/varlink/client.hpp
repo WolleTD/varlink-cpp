1f01d9226c3d1a2da50207d09cce43ce8821b3a1
Eicke Herbertz <wolletd@posteo.de>
2020-09-26 22:30:25 +0200

    Split out streaming json connection from socket stuff

--- include/varlink/client.hpp
+++ include/varlink/client.hpp
@@ -18,51 +18,29 @@ namespace varlink {
         return {std::error_code(errno, std::system_category()), what};
     }
 
-    class SocketConnection {
+    class StreamingConnection {
     private:
-        int socket_fd{-1};
-        __gnu_cxx::stdio_filebuf<char> filebuf_in;
-        __gnu_cxx::stdio_filebuf<char> filebuf_out;
-
-        std::istream rstream{&filebuf_in};
-        std::ostream wstream{&filebuf_out};
+        std::istream rstream;
+        std::ostream wstream;
     public:
-        // Connect to a service via address
-        explicit SocketConnection(const std::string &address) {
-            socket_fd = socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
-            if (socket_fd < 0) {
-                throw systemErrorFromErrno("socket() failed");
-            }
-            struct sockaddr_un addr { AF_UNIX , "" };
-            if (address.length() + 1 > sizeof(addr.sun_path)) {
-                throw std::system_error{std::make_error_code(std::errc::filename_too_long)};
-            }
-            address.copy(addr.sun_path, address.length());
-            if (connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-                throw systemErrorFromErrno("connect() failed");
-            }
-            filebuf_in = __gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::in);
-            filebuf_out = __gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::out);
+        template<typename BufferT>
+        StreamingConnection(BufferT* input, BufferT* output) : rstream(input), wstream(output) {}
+
+        StreamingConnection(const StreamingConnection &src) = delete;
+        StreamingConnection &operator=(const StreamingConnection &) = delete;
+
+        StreamingConnection(StreamingConnection &&src) noexcept :
+        rstream(src.rstream.rdbuf()),
+        wstream(src.wstream.rdbuf()) {
+            src.rstream.rdbuf(nullptr);
+            src.wstream.rdbuf(nullptr);
         }
 
-        // Setup message stream on existing connection
-        explicit SocketConnection(int posix_fd) : socket_fd(posix_fd),
-                filebuf_in(__gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::in)),
-                filebuf_out(__gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::out)) {}
-
-        SocketConnection(const SocketConnection &src) = delete;
-        SocketConnection &operator=(const SocketConnection &) = delete;
-
-        SocketConnection(SocketConnection &&src) noexcept :
-                socket_fd(std::exchange(src.socket_fd, -1)),
-                filebuf_in(std::move(src.filebuf_in)),
-                filebuf_out(std::move(src.filebuf_out)) {}
-
-        SocketConnection &operator=(SocketConnection &&rhs) noexcept {
-            SocketConnection c(std::move(rhs));
-            std::swap(socket_fd, c.socket_fd);
-            std::swap(filebuf_in, c.filebuf_in);
-            std::swap(filebuf_out, c.filebuf_out);
+        StreamingConnection &operator=(StreamingConnection &&rhs) noexcept {
+            rstream.rdbuf(rhs.rstream.rdbuf());
+            wstream.rdbuf(rhs.wstream.rdbuf());
+            rhs.rstream.rdbuf(nullptr);
+            rhs.wstream.rdbuf(nullptr);
             return *this;
         }
 
@@ -87,6 +65,53 @@ namespace varlink {
                     throw systemErrorFromErrno("Reading from stream failed");
                 }
             }
+        }
+    };
+
+    class SocketConnection : public StreamingConnection {
+    private:
+        int socket_fd{-1};
+        __gnu_cxx::stdio_filebuf<char> filebuf_in;
+        __gnu_cxx::stdio_filebuf<char> filebuf_out;
+    public:
+        // Connect to a service via address
+        explicit SocketConnection(const std::string &address) : StreamingConnection(&filebuf_in, &filebuf_out) {
+            socket_fd = socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
+            if (socket_fd < 0) {
+                throw systemErrorFromErrno("socket() failed");
+            }
+            struct sockaddr_un addr { AF_UNIX , "" };
+            if (address.length() + 1 > sizeof(addr.sun_path)) {
+                throw std::system_error{std::make_error_code(std::errc::filename_too_long)};
+            }
+            address.copy(addr.sun_path, address.length());
+            if (connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+                throw systemErrorFromErrno("connect() failed");
+            }
+            filebuf_in = __gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::in);
+            filebuf_out = __gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::out);
+        }
+
+        // Setup message stream on existing connection
+        explicit SocketConnection(int posix_fd) : StreamingConnection(&filebuf_in, &filebuf_out), socket_fd(posix_fd),
+                filebuf_in(__gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::in)),
+                filebuf_out(__gnu_cxx::stdio_filebuf<char>(socket_fd, std::ios::out)) {}
+
+        SocketConnection(const SocketConnection &src) = delete;
+        SocketConnection &operator=(const SocketConnection &) = delete;
+
+        SocketConnection(SocketConnection &&src) noexcept :
+                StreamingConnection(&filebuf_in, &filebuf_out),
+                socket_fd(std::exchange(src.socket_fd, -1)),
+                filebuf_in(std::move(src.filebuf_in)),
+                filebuf_out(std::move(src.filebuf_out)) {}
+
+        SocketConnection &operator=(SocketConnection &&rhs) noexcept {
+            SocketConnection c(std::move(rhs));
+            std::swap(socket_fd, c.socket_fd);
+            std::swap(filebuf_in, c.filebuf_in);
+            std::swap(filebuf_out, c.filebuf_out);
+            return *this;
         }
     };
 
